b80e31a31320d475f5415f7d2228403f351a342e
Rontim <rongitonga@gmail.com>
2023-04-01 20:27:55 +0300

    Added a view for making the connection & finished the query

--- ghost/views.py
+++ ghost/views.py
@@ -5,12 +5,14 @@ from django.views import generic
 from .forms import dbDetails
 import mysql.connector
 
+
 def index(request):
     context = {}
     context['form'] = dbDetails()
     return render(request, 'ghost/index.html', context)
 
-def ghost_worker(request):
+
+def connection_(request):
     if request.method == 'POST':
         dbName = request.POST['dbName']
         dbUser = request.POST['dbUser']
@@ -22,3 +24,31 @@ def ghost_worker(request):
         password=dbPassword,
         database=dbName
     )
+    return mydb
+
+
+def ghost_worker(request):
+    mydb = connection_()
+    ghost_workers = []
+    with mydb.cursor() as cursor:
+        cursor.execute("""
+            SELECT e.* w.employee_id, w.task, w.duration, w,status, w.comments
+            FROM employee e
+            LEFT JOIN worklog w ON e.employee_id = w.employee_id
+        """)
+
+        data = cursor.fetchall()
+
+        for data in data:
+            if (not data[5] or not data[6] or not data[7] or not data[7]) or (7):
+                employee = {
+                    'id': data[0],
+                    'name': data[1],
+                    'email': data[2],
+                    'department': data[3],
+                    'position': data[4],
+                }
+
+                ghost_workers.append(employee)
+
+    return render(request, 'ghost_workers.html', {'ghost_workers': ghost_workers})
